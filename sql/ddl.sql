f210e3a8fc5d6e2bb2a79715bdf2c73c6b37547a
Lariah Christensen <lariah_14@hotmail.com>
2019-07-16 16:42:49 -0600

    added in phase 3 - ddl

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -1,47 +1,32 @@
-drop table if exists articleTag;
-drop table if exists article;
+drop table if exists userID;
+drop table if exists photoTag;
 drop table if exists tag;
-drop table if exists author;
+drop table if exists photoComment;
 
-create table author(
-	authorId binary(16) not null,
-	authorAvatarUrl varchar(255),
-	authorActivationToken char(32),
-	authorEmail varchar(128) not null,
-	authorHash char(97) not null,
-	authorUsername varchar(32) not null,
-	unique(authorEmail),
-	unique(authorUsername),
-	INDEX(authorEmail),
-	primary key(authorId)
+create table user(
+	userId binary(16) not null,
+	userName varchar(255), not null,
+	userInfo char(32),
+	userProfilePicture varchar(128),
+	UNIQUE key(userId)
 );
 
 
-create table tag(
-	tagId binary(16) not null,
-	tagName varchar(32) not null,
-	primary key(tagId)
+create table photo(
+	photoUserID binary(16) not null,
+	photoCaption varchar(32)
+	photoComment varchar (32)
+	photoLocation char (32)
+	photoTag varchar (35)
+	INDEX (photoUserID)
+	foreign key (photoUserID) references (userID)
+	primary key (userID)
 );
 
-create table article(
-	articleId binary(16) not null,
-	articleAuthorId binary(16) not null,
-	articleContent varchar(40000) not null,
-	articleDate datetime(6) not null,
-	articleImage VARCHAR(255),
-
-	index(articleAuthorId),
-	foreign key(articleAuthorId) references author(authorId),
-	primary key(articleId)
-);
-
-create table articleTag(
-	articleTagArticleId binary(16),
-	articleTagTagId binary(16),
-
-	index(articleTagArticleId),
-	index(articleTagTagId),
-	foreign key(articleTagArticleId) references article(articleId),
-	foreign key(articleTagTagId) references tag(tagId),
-	primary key(articleTagArticleId, articleTagTagId)
+create table like (
+	likeUserId binary(16) not null,
+	likePhotoId binary(16) not null,
+	index(likeUserIdId),
+	foreign key(likeUserId) references user(userId),
+	foreign key (likePhotoID) references photo (photoUserID)
 );
