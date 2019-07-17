869d065da0a168d10f952a8f1c26e27ba527f5b3
Lariah Christensen <lariah_14@hotmail.com>
2019-07-17 11:23:24 -0600

    added in phase 3 - ddl ((updated))

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -1,31 +1,33 @@
+drop table if exists 'like';
 drop table if exists user;
 drop table if exists photo;
-drop table if exists like;
 
 create table user(
 	userId binary(16) not null,
 	userName varchar(255), not null,
-	userInfo char(32),
+	userInfo varchar(120),
 	userProfilePicture varchar(128),
 	UNIQUE key(userId)
+	primary key (userId)
 );
 
 
 create table photo(
+	photoID binary (20) not null,
 	photoUserID binary(16) not null,
 	photoCaption varchar(32)
 	photoComment varchar (32)
-	photoLocation char (32)
+	photoLocation varchar (32)
 	photoTag varchar (35)
 	INDEX (photoUserID)
 	foreign key (photoUserID) references (userID)
-	primary key (userID)
+	primary key (photoID)
 );
 
-create table like (
+create table 'like' (
 	likeUserId binary(16) not null,
 	likePhotoId binary(16) not null,
-	index(likeUserIdId),
+	index(likeUserId),
 	foreign key(likeUserId) references user(userId),
-	foreign key (likePhotoID) references photo (photoUserID)
+	foreign key (likePhotoID) references photo (photoID)
 );
